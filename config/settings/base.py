5f7d6783d3c4d5922d87a33e00fe3c90eb55a701
Dav2dAnderson <dav2danderson@gmail.com>
2026-07-20 21:31:52 +0500

    add: celery configuration

--- config/settings/base.py
+++ config/settings/base.py
@@ -274,7 +274,6 @@ CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 
 
 
-
 # --------------------------------------------------------------------------
 # Custom project settings
 # --------------------------------------------------------------------------
